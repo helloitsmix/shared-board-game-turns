9198ebb133bcc1aa0ec2fa42bbb22fa96a570b61
helloitsmix <lorenzovezzaro@yahoo.it>
2025-03-15 13:38:35 +0100

    changed config

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
 const config = {
-  IP: '192.168.1.105', // IP of your local network (IPv4)
+
+  // IP address and port number
+  IP: '192.168.1.105', // IPv4 of your local network
   PORT: 3000,
+
+  // List of rooms
   ROOMS: [
     'HEROQUEST',
     'OTHER'
   ]
+
 }
 
 module.exports = config
